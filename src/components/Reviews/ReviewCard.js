c501f129d569eee0e2f51895f74df3a3fe9d419b
md masud rana <mr7123619@gmail.com>
2023-01-13 02:17:35 +0600

    change font size of review card

--- src/components/Reviews/ReviewCard.js
+++ src/components/Reviews/ReviewCard.js
@@ -7,7 +7,7 @@ const ReviewCard = ({ review }) => {
     <div data-aos="zoom-in" data-aos-delay="50" data-aos-easing="ease-in-out">
       <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
         <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gradient-to-b from-blue-400 to-transparent h-72">
-          <p className="relative px-6 py-1 text-lg italic text-center text-black">
+          <p className="relative px-6 py-1 text-sm italic text-center text-black">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
